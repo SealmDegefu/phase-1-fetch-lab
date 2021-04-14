7fdb952a1a78761d63311ccc90589edd8b8ee0a7
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-04-13 20:37:45 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  const url = "https://anapioficeandfire.com/api/books"
+  return fetch(url).then(res => res.json()).then(renderBooks)
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const h3 = document.createElement('h3');
+    h3.innerHTML = book.authors;
+    main.appendChild(h3);
   });
 }
 
